3f3aae9690ef0c925c9734c85018a87f6f6581d0
Tirtha Biswas <tirthabiswasm@gmail.com>
2024-10-09 18:54:25 +1100

    Added semantic tags

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Footer from './components/Footer';
+import Main from './components/Main';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <Nav/>
+    <Main/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
